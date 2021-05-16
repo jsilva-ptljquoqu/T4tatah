90d2064a4a4ac5a9ba52d6b25acc309dc4e9e515
jsilva-ptljquoqu <jiliaozhan33584799@163.com>
2021-05-16 15:38:03 +0800

    first commit!

---
+++ src/com/example/hw2_callme/Callpan_activity.java
@@ -0,0 +1,18 @@
+package com.example.hw2_callme;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Callpan_activity extends Activity {
+	  @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+	    }
+	 
+
+}
